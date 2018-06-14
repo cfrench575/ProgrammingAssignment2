0100d9c652cb9c9b9de1ef82cf5e3fe009dd4cc4
Chelsea <cfrench575@gmail.com>
2018-06-13 23:13:05 -0700

    coursera programming assignment 2 R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming assignment 2, lexical scoping. 
 
-## Write a short comment describing this function
+## Makes empty object for storing cached data from inverse matrix calculation (solve function)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) s <<- inverse
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of a matrix, stores the answer to solve() so its only computed once per matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setinverse(s)
+  s
 }
